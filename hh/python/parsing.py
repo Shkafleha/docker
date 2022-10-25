2707d48e8238353487faa7c03782939e6cf9331b
Shkafleha <72297369+Shkafleha@users.noreply.github.com>
2022-10-25 23:40:16 +0500

    Add files via upload

---
+++ hh/python/parsing.py
@@ -0,0 +1,24 @@
+# https://dev.mysql.com/doc/connector-python/en/connector-python-example-cursor-select.html
+import mysql.connector
+
+config = {
+  'user': 'root',
+  'password': 'root',
+  'host': 'mysql',
+  'database': 'db',
+  'port': "3306"
+}
+connection = mysql.connector.connect(**config)
+print('DB connected')
+cursor = connection.cursor()
+cursor.execute("insert into hh(Amount) values(25)")
+print('New attempt')
+cursor.execute("SELECT * FROM hh")
+data = cursor.fetchall()
+# Make the changes to the database persistent
+connection.commit()
+
+# Close communication with the database
+cursor.close()
+connection.close()
+print(data)
